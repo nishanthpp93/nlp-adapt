bb8b95ff8ff049fd88775b46f1a80e091081a5cd
Nishanth Pavinkurve <npp267@nyu.edu>
2020-06-08 22:32:53 -0400

    Update Clamp version

--- config/clamp/run_nlpie_pipeline.sh
+++ config/clamp/run_nlpie_pipeline.sh
@@ -8,8 +8,8 @@ umlsPass=$ctakes_umlspw
 input=$DATA_IN
 output=$CLAMP_OUT
 
-clampbin="$CLAMP_HOME/bin/clamp-nlp-1.4.0-jar-with-dependencies.jar"
-pipeline="$CLAMP_HOME/pipeline/clamp-ner-attribute.pipeline.jar"
+clampbin="$CLAMP_HOME/bin/clamp-nlp-1.6.3-SNAPSHOT-jar-with-dependencies.jar"
+pipeline="$CLAMP_HOME/pipeline/clamp-ner.pipeline.jar"
 umlsIndex="$CLAMP_HOME/resource/umls_index/"
 
 java -DCLAMPLicenceFile="$CLAMP_HOME/CLAMP.LICENSE" -Xmx3g -cp $clampbin edu.uth.clamp.nlp.main.PipelineMain \
