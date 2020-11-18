0d9f79cc4c9636e075b777248e0a1f6be4ffe6c5
Nishanth Pavinkurve <npp267@nyu.edu>
2020-11-18 10:31:26 -0500

    Update output

--- scripts/run_metamap_lite.sh
+++ scripts/run_metamap_lite.sh
@@ -8,9 +8,7 @@ export JAVA_TOOL_OPTIONS='-Xms2G -Xmx6G -XX:MinHeapFreeRatio=25 -XX:+UseG1GC'
 
 pushd $METAMAPLITE_HOME
 for file in $DATA_IN/*.txt; do
-file_base_name=$(basename "${file}")
-file_name=${file_base_name%.*}
-cat "$file" | $METAMAPLITE_HOME/metamaplite.sh --pipe > $METAMAPLITE_OUT/"$file_name".csv
+cat "$file" | $METAMAPLITE_HOME/metamaplite.sh --pipe > $METAMAPLITE_OUT/"$file".csv
 done
 popd
 
