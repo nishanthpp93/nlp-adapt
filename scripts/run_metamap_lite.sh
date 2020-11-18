14ada020dcf2ffed3a792c1cda45d582936ac732
Nishanth Pavinkurve <npp267@nyu.edu>
2020-11-17 23:10:49 -0500

    Add suffix to file out

--- scripts/run_metamap_lite.sh
+++ scripts/run_metamap_lite.sh
@@ -8,7 +8,7 @@ export JAVA_TOOL_OPTIONS='-Xms2G -Xmx6G -XX:MinHeapFreeRatio=25 -XX:+UseG1GC'
 
 pushd $METAMAPLITE_HOME
 for file in $DATA_IN/*.txt; do
-cat $file | $METAMAPLITE_HOME/metamaplite.sh --pipe
+cat "$file" | $METAMAPLITE_HOME/metamaplite.sh --pipe > $METAMAPLITE_OUT/"$file"_out.txt
 done
 popd
 
