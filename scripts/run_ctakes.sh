4f577bbcf3357f1acfda1cdbcbc99f477b685eac
Nishanth Pavinkurve <npp267@nyu.edu>
2020-11-24 20:13:37 -0500

    Specify output option for ctakes

--- scripts/run_ctakes.sh
+++ scripts/run_ctakes.sh
@@ -6,6 +6,6 @@ set -x
 ##### Run cTAKES #####
 mkdir -p $CTAKES_OUT
 . $ADAPT_SCRIPTS/umls.sh
-$CTAKES_HOME/bin/runClinicalPipeline.sh -i $DATA_IN --xmiOut $CTAKES_OUT
+$CTAKES_HOME/bin/runClinicalPipeline.sh -i $DATA_IN -o $CTAKES_OUT
 
 set +x
